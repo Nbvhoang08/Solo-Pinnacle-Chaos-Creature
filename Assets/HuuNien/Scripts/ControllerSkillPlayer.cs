def435568dda0d9b9a05626fd0b9e41c43499e54
Nguyễn Hữu Niên <huunien734@gmail.com>
2023-12-05 16:36:39 +0700

    fix script ControllerSkillPlayer

--- Assets/HuuNien/Scripts/ControllerSkillPlayer.cs
+++ Assets/HuuNien/Scripts/ControllerSkillPlayer.cs
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-using System.Collections;
-=======
-﻿using System.Collections;
->>>>>>> main
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControllerSkillPlayer : MonoBehaviour
 {
-<<<<<<< HEAD
-    [SerializeField] private GameObject[] skills;
-    [SerializeField] private GameObject[] trans_Skill;
-    private List<int> starSkill;
-    private GameObject sk1, sk2, sk3;
-    private int skill1, skill2, skill3;
-    void Start()
-    {
-        this.gameObject.SetActive(false);
-        starSkill = new List<int>();
-        for(int i = 0; i < skills.Length; i++)
-        {
-            starSkill.Add(0);
-=======
     [SerializeField] private GameObject[] skillRed;
     [SerializeField] private GameObject[] skillBlue;
     [SerializeField] private GameObject[] trans_Skill; // list save position of 3 skill
@@ -37,58 +18,12 @@ public class ControllerSkillPlayer : MonoBehaviour
 
         for(int i = 0; i< skillRed.Length; i++)
             skills.Add(new Skill(i, skillRed[i].gameObject.name, skillRed[i], 0, "red"));
-        for(int i = 0;i< skillBlue.Length; i++)
-<<<<<<< Updated upstream
-        {
-            skills.Add(skillBlue[i]);
-        }
         for(int i = 0; i<skillBlue.Length + skillRed.Length; i++)
-        {
-            starSkills.Add(0);
->>>>>>> main
-        }
-=======
             skills.Add(new Skill(skillRed.Length + i, skillBlue[i].gameObject.name, skillBlue[i], 0, "blue"));
->>>>>>> Stashed changes
     }
 
     public void instanSkill()
     {
-<<<<<<< HEAD
-        skill1 = Random.Range(0, skills.Length);
-        skill2 = Random.Range(0, skills.Length);
-        skill3 = Random.Range(0, skills.Length);
-        while(skill1 == skill2) skill2 = Random.Range(0, skills.Length);
-        while (skill2 == skill3 || skill3 == skill1) skill3 = Random.Range(0, skills.Length);
-     /*   Debug.Log(skill1);
-        Debug.Log(skill2); 
-        Debug.Log(skill3);*/
-        sk1 = Instantiate(skills[skill1], trans_Skill[0].transform.position, Quaternion.identity, trans_Skill[0].transform);
-        sk1.transform.Find("Star").gameObject.GetComponent<Text>().text = starSkill[skill1].ToString() + " Star";
-        sk2 = Instantiate(skills[skill2], trans_Skill[1].transform.position, Quaternion.identity, trans_Skill[1].transform);
-        sk2.transform.Find("Star").gameObject.GetComponent<Text>().text = starSkill[skill2].ToString() + " Star";
-        sk3 = Instantiate(skills[skill3], trans_Skill[2].transform.position, Quaternion.identity, trans_Skill[2].transform);
-        sk3.transform.Find("Star").gameObject.GetComponent<Text>().text = starSkill[skill3].ToString() + " Star";
-        Time.timeScale = 0;
-    }
-    public void onClickChooseSkill(int i)
-    {
-        Time.timeScale = 1;
-        if (i == 1)
-        {
-            starSkill[skill1]++;
-        }else if(i == 2)
-        {
-            starSkill[skill2]++;
-        }
-        else
-        {
-            starSkill[skill3]++;
-        }
-        Destroy(sk1);
-        Destroy(sk2);
-        Destroy(sk3);
-=======
         indexChoose = new List<int>(); // làm mới ds lưu index skill được random
 
         foreach(var o in randomSkill())
@@ -151,11 +86,6 @@ public class ControllerSkillPlayer : MonoBehaviour
         // xóa 3 skill vừa hiện
         foreach (var a in trans_Skill)
             Destroy(a.gameObject.transform.GetChild(0).gameObject);
-<<<<<<< Updated upstream
-        }
->>>>>>> main
-=======
->>>>>>> Stashed changes
         this.gameObject.SetActive(false);
     }
 }
